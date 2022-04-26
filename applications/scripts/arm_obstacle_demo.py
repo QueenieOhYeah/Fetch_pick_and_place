409febc1b1f958e9d49ca9dc194efd3a04b0bf32
Jack Bowen <jackbowen92@gmail.com>
2022-04-25 19:04:58 -0700

    Finish Lab 21

--- applications/scripts/arm_obstacle_demo.py
+++ applications/scripts/arm_obstacle_demo.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 from moveit_python import PlanningSceneInterface
+from geometry_msgs.msg import PoseStamped
 import robot_api
 import rospy
 
@@ -16,9 +17,8 @@ def main():
     rospy.init_node('arm_demo')
     wait_for_time()
 
-    # Instantiate the planning scene 
+    # Instantiate the planning scene
     planning_scene = PlanningSceneInterface('base_link')
-    print("got here")
 
     # Create table obstacle
     planning_scene.removeCollisionObject('table')
@@ -40,6 +40,61 @@ def main():
     y = 0 
     z = table_z + (table_size_z / 2) + (size_z / 2)
     planning_scene.addBox('divider', size_x, size_y, size_z, x, y, z)
+
+    arm = robot_api.Arm()
+    def shutdown():
+        arm.cancel_all_goals()
+    rospy.on_shutdown(shutdown)
+
+    kwargs = {
+        'allowed_planning_time': 15,
+        'execution_timeout': 10,
+        'num_planning_attempts': 5,
+        'replan': False
+    }
+
+    pose1 = PoseStamped()
+    pose1.header.frame_id = 'base_link'
+    pose1.pose.position.x = 0.5
+    pose1.pose.position.y = -0.3
+    pose1.pose.position.z = 0.75
+    pose1.pose.orientation.w = 1
+
+    pose2 = PoseStamped()
+    pose2.header.frame_id = 'base_link'
+    pose2.pose.position.x = 0.5
+    pose2.pose.position.y = 0.3
+    pose2.pose.position.z = 0.75
+    pose2.pose.orientation.w = 1
+
+    planning_scene.removeAttachedObject('tray')
+
+    error = arm.move_to_pose(pose1, **kwargs)
+    if error is not None:
+        rospy.logerr('Pose 1 failed: {}'.format(error))
+    else:
+        rospy.loginfo('Pose 1 succeeded')
+        frame_attached_to = 'gripper_link'
+        frames_okay_to_collide_with = [
+            'gripper_link', 'l_gripper_finger_link', 'r_gripper_finger_link'
+        ]
+        planning_scene.attachBox('tray', 0.3, 0.07, 0.01, 0.05, 0, 0,
+                                frame_attached_to, frames_okay_to_collide_with)
+        planning_scene.setColor('tray', 1, 0, 1)
+        planning_scene.sendColors()
+
+    rospy.sleep(1)
+    error = arm.move_to_pose(pose2, **kwargs)
+    if error is not None:
+        rospy.logerr('Pose 2 failed: {}'.format(error)) 
+    else:
+        rospy.loginfo('Pose 2 succeeded')
+
+    planning_scene.removeCollisionObject('table')
+    planning_scene.removeCollisionObject('divider')
+
+    # At the end of your program
+    planning_scene.removeAttachedObject('tray')
 
 if __name__ == '__main__':
     main()
